b0f374495b1085b552d0f767639d4428c71d203c
Nguyen Thanh Thuc <nguyen.thanh.rcvn2012@gmail.com>
2022-03-08 17:03:50 +0700

    add

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('/users', [UserController::class, 'index'])->name('users.all');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+Route::put('/users', [UserController::class, 'update'])->name('users.update');
+Route::delete('/users', [UserController::class, 'destroy'])->name('users.destroy');
